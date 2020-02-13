d28a1c7cc20d6f4abba4e5701a51e2590495f9e4
Lucas Coffee <lsterlingcoffee@gmail.com>
2020-02-12 21:06:43 -0800

    more directory assembly. Also app.js prelim assembly

--- server/app/src/App.js
+++ server/app/src/App.js
@@ -1,25 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import Home from "./pages/Home";
+import Cart from "./pages/Cart";
+import { CartProvider } from "./utils/CartState";
+import { ProductsProvider } from "./utils/ProductState";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <CartProvider>
+      <ProductsProvider>
+        <Router>
+          <div>
+            <NavBar />
+            <Route exact path="/" component={Home} />
+
+            <Route exact path="/cart" component={Cart} />
+          </div>
+        </Router>
+      </ProductsProvider>
+    </CartProvider>
   );
 }
 
