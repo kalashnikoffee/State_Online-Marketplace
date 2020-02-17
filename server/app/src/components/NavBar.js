a842ae90f071860d5707ae8756ccd26283da6a40
Lucas Coffee <lsterlingcoffee@gmail.com>
2020-02-16 19:31:22 -0800

    quick fix to labels

--- server/app/src/components/NavBar.js
+++ server/app/src/components/NavBar.js
@@ -8,7 +8,7 @@ function NavBar({ isLoggedIn }) {
 
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a className="navbar-brand" href="/">Best Buyzon</a>
+            <a className="navbar-brand" href="/">Best Buy Buy</a>
             <div className="collapse navbar-collapse" id="navbarNav">
                 <ul className="navbar-nav">
                     <li className="nav-item">
